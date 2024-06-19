b151716ffe139fababd9cdeb06ad813736e74aea
Skak67 <adamhappyslalu836@gmail.com>
2024-06-19 10:08:45 +0700

    update BGM mainmenu dan Gameplay

--- UJikom/Assets/BGMMainMenu.cs
+++ UJikom/Assets/BGMMainMenu.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGMController : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        PlayBGM();
+        
     }
 
     // Update is called once per frame
-    public void PlayBGM(){
-        audioSource.Play();
+    void Update()
+    {
+        
     }
-
 }
--- UJikom/Assets/MainMenu Controller.cs
+++ UJikom/Assets/MainMenu Controller.cs
@@ -6,21 +6,34 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public AudioSource audioSource;
     public Button PlayButton;
     public Button ExitButton;
        void Start()
     {
         PlayButton.onClick.AddListener(PlayGame);
         PlayButton.onClick.AddListener(ExitGame);
+        PlayBGM();
         
     }
 
     public void PlayGame(){
         SceneManager.LoadScene("GamePlay");
+        StopBGM();
+
     }
 
     public void ExitGame(){
         Application.Quit();
+    }
+
+    public void PlayBGM(){
+        audioSource.Play();
+    }
+
+    public void StopBGM(){
+        audioSource.Stop();
+
     }
 
 
